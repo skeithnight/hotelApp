4af2bb5afbd2d738671cd1729b8e1c45f946971a
dwiki nugraha <skeithnight@gmail.com>
2018-10-19 05:51:48 +0700

    login add shareprefence

--- app/src/main/java/com/macbook/puritomat/activity/LoginActivity.kt
+++ app/src/main/java/com/macbook/puritomat/activity/LoginActivity.kt
@@ -1,13 +1,36 @@
 package com.macbook.puritomat.activity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 import com.macbook.puritomat.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    fun submit(){
+        val mSPLogin = getSharedPreferences("Login", Context.MODE_PRIVATE)
+        val editor = mSPLogin.edit()
+
+        editor.putString(et_username_login.text.toString(), "userKos")
+        editor.putString(et_password_login.text.toString(), "pasKos")
+        editor.putString("token", "tokenKos")
+        editor.apply()
+
+        val intent = Intent(this@LoginActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/macbook/puritomat/activity/MainActivity.java
+++ app/src/main/java/com/macbook/puritomat/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.macbook.puritomat.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -13,6 +16,9 @@ import com.appus.splash.Splash;
 import com.macbook.puritomat.R;
 
 public class MainActivity extends AppCompatActivity {
+
+//    SharedPreferences
+    SharedPreferences mSPLogin;
 
     private TextView mTextMessage;
 
@@ -45,28 +51,44 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        initializeSP();
+
         startSplashScreen();
 
     }
 
-//    to start splash screen
+    private void initializeSP() {
+         mSPLogin = getSharedPreferences("Login", Context.MODE_PRIVATE);
+         SharedPreferences.Editor editor = mSPLogin.edit();
+         editor.clear();
+    }
+
+    //    to start splash screen
     public void startSplashScreen(){
         Splash.Builder splash = new Splash.Builder(MainActivity.this, getSupportActionBar());
         splash.setOneShotStart(true);
         splash.perform();
-
+        CheckLogin();
 
 //        after splash execute routing login
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
+//                CheckLogin();
             }
         }, 3000);
 
     }
 
+    private void CheckLogin() {
+        String token = mSPLogin.getString("token",null);
+        Log.i("testing", "CheckLogin: "+ token);
+        if (token == null){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
 
 }
