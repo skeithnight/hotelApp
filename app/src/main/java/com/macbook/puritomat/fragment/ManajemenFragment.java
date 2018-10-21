9685416d765cb06e0a36e259592f64bb9cf7beb6
dwiki nugraha <skeithnight@gmail.com>
2018-10-21 12:21:35 +0700

    login add shareprefence

--- app/src/main/java/com/macbook/puritomat/fragment/ManajemenFragment.java
+++ app/src/main/java/com/macbook/puritomat/fragment/ManajemenFragment.java
@@ -3,15 +3,18 @@ package com.macbook.puritomat.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.macbook.puritomat.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,4 +42,26 @@ public class ManajemenFragment extends Fragment {
         return view;
     }
 
+    @OnClick({R.id.cdManajemen1,R.id.cdManajemen2,R.id.cdManajemen3,R.id.btn_logout})
+    public void clickCV(View view){
+        switch (view.getId()) {
+            case R.id.cdManajemen1:
+                // do something
+                Toast.makeText(view.getContext(), "1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cdManajemen2:
+                // do something else
+                Toast.makeText(view.getContext(), "2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cdManajemen3:
+                // i'm lazy, do nothing
+                Toast.makeText(view.getContext(), "3", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_logout:
+                // i'm lazy, do nothing
+                Toast.makeText(view.getContext(), "logout", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
 }
